a8190138fc48a00e11aa129bda559e666702c86d
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2021-04-17 13:55:23 -0700

    Applied automated CS fixes

--- src/Lap.php
+++ src/Lap.php
@@ -16,8 +16,8 @@ class Lap
     /**
      * Create a new Lap object.
      *
-     * @param float  $time        The current time
-     * @param float  $duration    The duration of the lap
+     * @param float $time The current time
+     * @param float $duration The duration of the lap
      * @param string $description A description of the lap
      */
     public function __construct(float $time, float $duration, string $description = '')
